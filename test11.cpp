7eb8d12bcb9d0cc140760fcb2b43b22b0809d7f0
Jaeyun-18 <59586828+Jaeyun-18@users.noreply.github.com>
2020-04-18 17:55:20 +0900

    최재민이 만듬

--- test11.cpp
+++ test11.cpp
@@ -1,9 +1,52 @@
 #include <stdio.h>
-
+#include <string.h>
+#include <math.h>
 int main()
 {
-	int n[3][4] = {{0,1,2},{3,4,5},{6,7,8}};
-	printf("%d", n[1][2]);
-	
-	return 0;
+    char a[1000000], b[1000000];
+    int i;
+    scanf("%s", &a);
+    int result=0, t, second=0;
+    int length=0, start=0, finish;
+    while(a[start]>='0' && a[start]<='9'){
+    	b[start]=a[length];
+    	length++;
+    	start++;
+	}
+	finish=start;
+	for(i=0; i<length; i++){
+		result=result+pow(10, i)*(b[start-1]-48);
+		start--;
+	}
+	start=finish+2;
+	length=0;
+    while(a[start-1]!='=')
+    {
+    	while(a[start]>='0' && a[start]<='9'){
+    	    b[length]=a[start];
+    	    length++;
+    	    start++;
+	    }
+	    finish=start;
+	    for(i=0; i<start; i++){
+		    second=second+pow(10, i)*(b[length-1]-48);
+	        start--;
+	    }
+        if(a[start-1]=='+'){
+            result+=second;
+        }
+        else if(a[start-1]=='-'){
+            result-=second;
+        }
+        else if(a[start-1]=='*'){
+            result*=second;
+        }
+        else if(a[start-1]=='/'){
+            result/=second;
+        }
+        start=finish+2;
+	    length=0;
+        second=0;
+    }
+    printf("%d", result);
 }
